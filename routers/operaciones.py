f17a449951a6da4eb406021ce8160040a5daad45
Jairodaniel-17 <20b3010069@untels.edu.pe>
2024-11-17 18:36:07 -0500

    json y parametros como ejemplo de endpoints

--- routers/operaciones.py
+++ routers/operaciones.py
@@ -5,6 +5,11 @@ from models.suma import Suma
 router = APIRouter()
 
 
-@router.post("/suma")
+@router.post("/suma")  # entrada como JSON
 async def suma(suma: Suma):
     return {"resultado": suma.a + suma.b}
+
+
+@router.get("/suma")  # entrada como query params
+async def suma(a: int, b: int):
+    return {"resultado": a + b}
